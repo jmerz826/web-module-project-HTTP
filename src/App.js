3eb0cd1b739a2f47b5bf7e6a4b76928c8f43c0fd
John Merz <jmerz826@gmail.com>
2021-12-15 20:46:33 -0500

    done.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import FavoriteMovieList from './components/FavoriteMovieList';
 import EditMovieForm from "./components/EditMovieForm";
 
 import axios from 'axios';
+import AddMovieForm from "./components/AddMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
@@ -52,6 +53,13 @@ const App = (props) => {
 
             <Route path="/movies/:id">
               <Movie setMovies={setMovies} deleteMovie={ deleteMovie}/>
+            </Route>
+
+            <Route path='/movies/add'>
+              <AddMovieForm
+                {...props}
+                setMovies={setMovies}
+              />
             </Route>
 
             <Route path="/movies">
